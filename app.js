9f4b5c3b0875cc1938c0405c3bb90a6f09a665cd
Brayden DeLoach <brayden@deloach.us>
2020-02-24 12:08:43 -0600

    Added feature to view employees, roles, and departments.

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ module.exports.handler = async function(){
     switch(command.command){
         
         case("End Program"): query.endQuery(); break;
-        
+        case("View All Employees"): query.viewEmployees(); break;
+        case("View All Departments"): query.viewDepartments(); break;
+        case("View All Roles"): query.viewRoles(); break;
     }
 }

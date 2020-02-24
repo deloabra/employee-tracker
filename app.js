563aa3004fd70f175a67db9796fa98bb86c5b0c2
Brayden DeLoach <brayden@deloach.us>
2020-02-24 12:20:36 -0600

    Created addDepartment function in query.js. Update other js files to use it.

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const questions = require("./lib/questions");
 const query = require("./lib/query");
 
+//Create connection in query.js
+//handler gets called at the end of init
 query.init();
 
 module.exports.handler = async function(){
     
+    //Get command from questions.js
     const command = await questions.getCommand();
     
+    //Call the query function specified by the user
+    //handler gets called at the end of each of these functions
     switch(command.command){
         
         case("End Program"): query.endQuery(); break;
         case("View All Employees"): query.viewEmployees(); break;
         case("View All Departments"): query.viewDepartments(); break;
         case("View All Roles"): query.viewRoles(); break;
+        case("Add a Department"): query.addDepartment(); break;
+
     }
 }

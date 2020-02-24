4ef60b0032a2162419d6971cee52d69a6427296b
Brayden DeLoach <brayden@deloach.us>
2020-02-24 14:39:47 -0600

    Added a project constructor to connection.query and updated every query to use promises.

--- seed.sql
+++ seed.sql
@@ -22,3 +22,17 @@ CREATE TABLE department(
     id int not null auto_increment primary key,
     name varchar(30)
 );
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+values
+("Brayden", "DeLoach", 1, null),
+("Charlie", "DeLoach", 2, 1);
+
+INSERT INTO role(title, salary, department_id)
+values
+("Student", 0, 1),
+("Cat", 10000, 1);
+
+INSERT INTO department(name)
+values
+("Temp");
